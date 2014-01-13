644e86e04827e17d28c033fade4c86b4147b7490
Oleksandr Bodnarashyk <boda2004@gmail.com>
2014-01-13 16:44:36 +0200

    update tests to pass

---
+++ src/HouseFinder/ParserBundle/Tests/Service/SlandoServiceTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: boda
+ * Date: 13.01.14
+ * Time: 16:04
+ */
+
+namespace HouseFinder\ParserBundle\Tests\Service;
+
+
+class SlandoServiceTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAllOk() {
+        $this->assertTrue(true);
+    }
+
+}
