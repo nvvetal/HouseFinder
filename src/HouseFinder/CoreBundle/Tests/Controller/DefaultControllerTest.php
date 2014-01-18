3c7970bb74cd57654caf5e61a3f48c618d99e2b4
boda <boda2004@gmail.com>
2014-01-18 09:57:15 +0200

    more reliable google routing test data

--- src/HouseFinder/CoreBundle/Tests/Controller/DefaultControllerTest.php
+++ src/HouseFinder/CoreBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,7 +10,7 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/route/Zhytomyr/Kyiv');
+        $crawler = $client->request('GET', '/route/Житомир,%20пл.%20Соборная,%201/Житомир,%20ул.%20Чапаева,%207');
         $this->assertTrue($crawler->filter('html:contains("Status:OK")')->count() > 0);
     }
 }
