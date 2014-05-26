2482eef20e5c08c4c165bed66ea574742910bdfe
vitaly.grinchishin <vitaly.grinchishin@silkcode.com>
2014-05-26 18:28:33 +0300

    added

---
+++ src/HouseFinder/CoreBundle/Entity/UserInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nv-vetal
+ * Date: 26.05.14
+ * Time: 17:49
+ */
+
+namespace HouseFinder\CoreBundle\Entity;
+
+
+interface UserInterface {
+    public function getUsernameFiltered();
+} 
