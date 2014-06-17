4bca4d52faa784eb17ccbe9f21e5190d3ab6d2c4
vitaly.grinchishin <vitaly.grinchishin@silkcode.com>
2014-06-17 18:21:46 +0300

    pager update

--- src/HouseFinder/FrontendBundle/Resources/public/js/common/directives/pager.js
+++ src/HouseFinder/FrontendBundle/Resources/public/js/common/directives/pager.js
@@ -31,11 +31,42 @@ angular.module('app').directive('appPager', function(){
                 return true;
             }
 
+            $scope.calcPrevNext = function (page, count){
+                $scope.prevPage = page > 0 ? page-1 : 0;
+                $scope.nextPage = page < count - 1 ? page+1 : count - 1;
+            }
+
+            $scope.getPagesRange = function(){
+                var maxShowPages = 10;
+                var range = [];
+                var from = 0;
+                var to = $scope.pages > maxShowPages ? maxShowPages : $scope.pages;
+
+                if($scope.currentPage == $scope.pages - 1){
+                    from = $scope.pages - maxShowPages - 1;
+                    to = $scope.pages;
+                }else if($scope.currentPage + 1 > maxShowPages - 1){
+                    from = $scope.currentPage - parseInt(maxShowPages / 2);
+                    to = $scope.pages > from + maxShowPages ? from + maxShowPages : $scope.pages;
+                }
+                for (var i = from; i < to; i++)
+                    range.push(i);
+                return range;
+            }
+
         }],
         link: function (scope, element, attrs) {
             scope.pagesAvailable = false;
+            scope.currentPage = 0;
+            scope.prevPage = 0;
+            scope.nextPage = 0;
+            scope.$watch('page', function(newValue, oldValue) {
+                scope.currentPage = parseInt(newValue);
+                scope.calcPrevNext(scope.currentPage, scope.pages);
+            });
             scope.$watch('pagesMax', function(newValue, oldValue) {
-                scope.refreshPages(newValue);
+                scope.refreshPages(parseInt(newValue));
+                scope.calcPrevNext(scope.currentPage, scope.pages);
             });
         }
     }
